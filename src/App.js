bce89688900a79063ab5ce925b2eb71bb7f71214
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-20 16:33:34 +0000

    Fri, Jan 20, 2023, 4:33 PM +00:00

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {AiFillApple} from "react-icons/ai"
 import Todo from "./Todo"
 
 const style = {
-  bg: `h-screen w-sscreen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500x] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text 3-l font-bold text-center text-gray-800 p-2`
+
 }
 
 
