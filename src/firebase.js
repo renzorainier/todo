2eac5226a1394832c149e7d866728df4112bbd55
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-20 16:52:58 +0000

    Fri, Jan 20, 2023, 4:52 PM +00:00

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_soIfP7wS1ryZOViuff21j7a7J47JAFs",
+  authDomain: "todo-41d9b.firebaseapp.com",
+  projectId: "todo-41d9b",
+  storageBucket: "todo-41d9b.appspot.com",
+  messagingSenderId: "706341846764",
+  appId: "1:706341846764:web:c71e45ecefbc928d60efc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
