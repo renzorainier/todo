652bedb19d2a92561364641bfe6e9b3e16c8d8b5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-01-21 15:28:27 +0000

    Sat, Jan 21, 2023, 3:28 PM +00:00

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,6 +8,8 @@ const style = {
   text: `ml-2 cursor-pointer`,
   textComplete: `ml-2 cursor-pointer line-through `,
   button: `cursor-pointer flex items-center`,
+
+
 };
 
 const Todo = ({ todo, toggleComplete, deleteTodo }) => {
